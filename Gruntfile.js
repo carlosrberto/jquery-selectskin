1a0c7519b558eb1c6186ebb1b351d17a1dd685fa
Carlos Roberto <carlos.rberto@gmail.com>
2013-08-31 16:01:44 -0300

    added some Grunt automation

---
+++ Gruntfile.js
@@ -0,0 +1,119 @@
+module.exports = function(grunt) {
+
+    grunt.initConfig({
+        pkg: grunt.file.readJSON('package.json'),
+        bower: grunt.file.readJSON('bower.json'),
+
+        options: {
+            banner: '' +
+            '/*!\n' +
+            ' * jQuery SelectSkin\n' +
+            ' * <%= pkg.homepage %>\n' +
+            ' *\n' +
+            ' * Copyright (c) <%= grunt.template.today("yyyy") %> <%= pkg.author.name %>\n' +
+            ' * <%= pkg.author.url %>\n' +
+            ' * \n' +
+            ' * Licensed under a <%= pkg.licenses[0].type %>\n' +
+            ' * <%= pkg.licenses[0].url %>\n' +
+            ' *\n' +
+            ' * Version: <%= pkg.version %>\n' +
+            ' */\n',
+
+            name: 'jquery.selectskin',
+        },
+
+        clean: ["dist"],
+
+        jquerymanifest: {
+            options: {
+                source: '<%= pkg %>',
+                overrides: {
+                    name: 'selectskin',
+                    dependencies: '<%= bower.dependencies %>'
+                }
+            }
+        },
+
+        concat: {
+            options: {
+                banner: '<%= options.banner %>'
+            },
+
+            js: {
+                src: ['src/<%= options.name %>.js'],
+                dest: 'dist/<%= options.name %>.js'
+            },
+
+            css: {
+                src: ['src/<%= options.name %>.css'],
+                dest: 'dist/<%= options.name %>.css'
+            }
+        },
+
+        uglify: {
+            options: {
+                banner: '<%= options.banner %>'
+            },
+
+            dist: {
+                files: {
+                  'dist/<%= options.name %>.min.js': ['src/<%= options.name %>.js']
+                }
+            }
+        },
+
+        cssmin: {
+            options: {
+                banner: '<%= options.banner %>'
+            },
+
+
+
+            dist: {
+                files: {
+                   'dist/<%= options.name %>.min.css': ['src/<%= options.name %>.css']
+                }
+            }
+        },
+
+        jshint: {
+            all: ['Gruntfile.js', 'src/**/*.js']
+        },
+
+        watch: {
+            scripts: {
+                files: ['src/*.js'],
+                tasks: ['jshint']
+            }
+        },
+
+        connect: {
+            demo: {
+                options: {
+                    port: 8000
+                }
+            }
+        },
+
+        open: {
+            demo: {
+                path: 'http://localhost:8000/demo/',
+                app: 'Google Chrome'
+            }
+        }
+    });
+
+    grunt.loadNpmTasks('grunt-contrib-clean');
+    grunt.loadNpmTasks('grunt-contrib-jshint');
+    grunt.loadNpmTasks('grunt-contrib-uglify');
+    grunt.loadNpmTasks('grunt-contrib-concat');
+    grunt.loadNpmTasks('grunt-contrib-cssmin');
+    grunt.loadNpmTasks('grunt-contrib-connect');
+    grunt.loadNpmTasks('grunt-contrib-watch');
+    grunt.loadNpmTasks('grunt-jquerymanifest');
+    grunt.loadNpmTasks('grunt-open');
+
+    // Default task(s).
+    grunt.registerTask('default', ['clean', 'jshint', 'jquerymanifest', 'concat', 'uglify', 'cssmin']);
+    grunt.registerTask('server', ['connect:demo', 'open:demo', 'watch']);
+};
